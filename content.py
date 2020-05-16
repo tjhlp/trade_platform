790da802c2eb01f06906c0b75c74770b9c8ad3b5
tjhlp <374586186@qq.com>
2020-05-16 19:59:02 +0800

    trade_platform add account_control -2

--- content.py
+++ content.py
@@ -1,19 +1,19 @@
 from flask import Flask
-from account.account_main import Account
+# from account.account_main import Account
 from user.user_main import User
 
-application = Flask(__name__)
-application.config['JSON_AS_ASCII'] = False
-application.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
-Account.setup(application)
-User.setup(application)
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+# Account.setup(application)
+User.setup(app)
+ 
 
-
-@application.route('/')
+@app.route('/')
 def home():
     return 'home_page'
 
 
 if __name__ == '__main__':
-    application.run(host='0.0.0.0', port=8002, debug=True, processes=1)
+    app.run(host='0.0.0.0', port=8002, debug=True, processes=1)
     # application.run(host='127.0.0.1', port=8002, debug=True, processes=1)
