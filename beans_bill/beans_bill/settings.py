8fca09ea63d40b95d38891ad0c8bd00ae280fa6e
tjhlp <374586186@qq.com>
2020-07-14 21:55:44 +0800

    add log

--- beans_bill/beans_bill/settings.py
+++ beans_bill/beans_bill/settings.py
@@ -155,7 +155,7 @@ LOGGING = {
         'file': {  # 向文件中输出日志
             'level': 'INFO',
             'class': 'logging.handlers.RotatingFileHandler',
-            'filename': os.path.join(os.path.dirname(BASE_DIR), 'logs/data_structure.log'),  # 日志文件的位置
+            'filename': os.path.join(os.path.dirname(BASE_DIR), 'logs/beans.log'),  # 日志文件的位置
             'maxBytes': 300 * 1024 * 1024,
             'backupCount': 10,
             'formatter': 'verbose'
