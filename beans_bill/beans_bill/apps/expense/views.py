aaeaec09f798562cf8d5e2afc813f75be3d2b12d
tjhlp <374586186@qq.com>
2020-07-19 21:58:31 +0800

    add expense

--- beans_bill/beans_bill/apps/expense/views.py
+++ beans_bill/beans_bill/apps/expense/views.py
@@ -18,16 +18,18 @@ class ExpenseListView(View):
         # 判断数据源是否属于所属节点
         req_params = {''}
         try:
-            info_models = ExpenseInfo.objects.filter(account_id=js['user_id'])
+            info_models = ExpenseInfo.objects.filter(user_id=js['user_id'])
         except Exception as error:
             logger.error("BillInfo got exception：%s, params:%s" % (str(error), js))
             return json_response(CODE_NODE_SOURCE_MISSING)
         res = []
         for info_model in info_models:
             rsp = {
-                'bill_name': info_model.bill_name,
-                'bill_members': info_model.bill_members,
-                'bill_auth': info_model.bill_auth
+                'expense_name': info_model.expense_name,
+                'expense_type': info_model.expense_type,
+                'expense_time': info_model.expense_time,
+                'expense_cost': info_model.expense_cost,
+                'expense_content': info_model.expense_content
             }
             res.append(rsp)
         return json_response(CODE_SUCCESS, res)
@@ -37,16 +39,20 @@ class ExpenseAddView(View):
     """ 添加消费记录"""
 
     def post(self, request):
-        params = {'user_id': (1, str), 'bill_id': (1, str), 'expense_name': (1, str), 'expense_type': (1, str),
+        params = {'user_id': (1, str), 'bill_id_list': (1, list), 'expense_name': (1, str), 'expense_type': (1, str),
                   'expense_time': (1, str), 'expense_cost': (1, str), 'expense_content': (1, str)}
         js, code = valid_body_js(request, params)
         if code != CODE_SUCCESS:
             logger.error("invalid param")
             return json_response(code)
 
-        ex_ = ExpenseInfo.objects.create(**js)
-
+        bill_id_list = js.pop('bill_id_list')
+        ex_list = []
+        for bill_id in bill_id_list:
+            js['bill_id'] = bill_id
+            ex_ = ExpenseInfo.objects.create(**js)
+            ex_list.append(ex_.expense_id)
         rsp = {
-            'expense_id': ex_.expense_id
+            'expense_id': ex_list
         }
         return json_response(CODE_SUCCESS, rsp)
