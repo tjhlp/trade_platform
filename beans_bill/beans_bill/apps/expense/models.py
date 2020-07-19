5757130e13f9980764e31f3826dbe0c9dcf98366
tjhlp <374586186@qq.com>
2020-07-19 21:41:11 +0800

    add expense

--- beans_bill/beans_bill/apps/expense/models.py
+++ beans_bill/beans_bill/apps/expense/models.py
@@ -8,11 +8,12 @@ class ExpenseInfo(BaseModel):
     """消费记录表"""
     expense_id = models.AutoField(verbose_name='表ID', primary_key=True)
     bill_id = models.IntegerField(verbose_name='账单记录id')
+    user_id = models.IntegerField(verbose_name='用户ID', default=0)
     expense_name = models.CharField(max_length=20, verbose_name='消费记录名')
     expense_type = models.CharField(max_length=20, verbose_name='消费类型')
     expense_time = models.DateTimeField(verbose_name='消费时间')
     expense_cost = models.DecimalField(max_digits=12, decimal_places=3, verbose_name='消费金额')
-    expense_content = models.CharField(max_length=100, verbose_name='消费备注')
+    expense_content = models.CharField(max_length=100, verbose_name='消费备注', default='')
 
     class Meta:
         db_table = 'TAB_EXPENSE'
--- beans_bill/beans_bill/apps/expense/views.py
+++ beans_bill/beans_bill/apps/expense/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from expense.models import *
+from beans_bill.utils.response_code import *
+from beans_bill.utils.comm_utils import *
+
+
+class ExpenseListView(View):
+    """ 显示账户下所有消费记录"""
+
+    def post(self, request):
+        params = {'user_id': (1, str), 'bill_name': (0, str), 'expense_type': (0, str), 'expense_time': (0, str)}
+        js, code = valid_body_js(request, params)
+        if code != CODE_SUCCESS:
+            logger.error("invalid param")
+            return json_response(code)
+
+        # 判断数据源是否属于所属节点
+        req_params = {''}
+        try:
+            info_models = ExpenseInfo.objects.filter(account_id=js['user_id'])
+        except Exception as error:
+            logger.error("BillInfo got exception：%s, params:%s" % (str(error), js))
+            return json_response(CODE_NODE_SOURCE_MISSING)
+        res = []
+        for info_model in info_models:
+            rsp = {
+                'bill_name': info_model.bill_name,
+                'bill_members': info_model.bill_members,
+                'bill_auth': info_model.bill_auth
+            }
+            res.append(rsp)
+        return json_response(CODE_SUCCESS, res)
+
+
+class ExpenseAddView(View):
+    """ 添加消费记录"""
+
+    def post(self, request):
+        params = {'user_id': (1, str), 'bill_id': (1, str), 'expense_name': (1, str), 'expense_type': (1, str),
+                  'expense_time': (1, str),
+                  'expense_cost': (1, str), 'expense_content': (1, str)}
+        js, code = valid_body_js(request, params)
+        if code != CODE_SUCCESS:
+            logger.error("invalid param")
+            return json_response(code)
+
+        ex_ = ExpenseInfo.objects.create(**js)
+
+        rsp = {
+            'expense_id': ex_.expense_id
+        }
+        return json_response(CODE_SUCCESS, rsp)
